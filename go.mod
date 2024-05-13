00cbd8aebb6ee95556e853372be7466c673cf544
Azka rizqullah <azkarizqullah99@gmail.com>
2024-05-13 16:59:05 +0700

    edit

--- go.mod
+++ go.mod
@@ -8,8 +8,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/gofiber/fiber v1.14.6 // indirect
-	github.com/gofiber/fiber/v2 v2.52.4 // indirect
+	github.com/gofiber/fiber/v2 v2.52.4 // 
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
@@ -28,5 +27,5 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
-	gorm.io/gorm v1.25.10 // indirect
+	gorm.io/gorm v1.25.10 
 )
--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -1,12 +1,39 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/AzkaAzkun/Note-App/config"
 	"github.com/AzkaAzkun/Note-App/models"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func Login(c *fiber.Ctx) error {
-	var user models.User
+	var userInput models.User
+	if err := c.BodyParser(&userInput); err != nil {
+		response := map[string]string{"message": err.Error()}
+		return c.Status(http.StatusBadRequest).JSON(response)
+	}
 
+	var user models.User
+	if err := config.DB.Where("username = ?", userInput.Username).First(&user).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			response := map[string]string{"message": "Username atau password salah"}
+			return c.Status(http.StatusUnauthorized).JSON(response)
+		default:
+			response := map[string]string{"message": err.Error()}
+			return c.Status(http.StatusInternalServerError).JSON(response)
+		}
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password)); err != nil {
+		response := map[string]string{"message": "Username atau password salah"}
+		return c.Status(http.StatusUnauthorized).JSON(response)
+	}
+
+	// expTime := time.Now().Add(time.Minute*1)
+	// claims
 	return nil
 }
